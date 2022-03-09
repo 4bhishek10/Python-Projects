0f1342cfd18793f77eb8b850d269587754446229
4bhishek10 <abhishek.dhondge10@gmail.com>
2022-03-09 14:53:40 +0530

    Bitonic sort test

--- Assignment set/test.py
+++ Assignment set/test.py
@@ -1,35 +1,53 @@
-import math
-import os
-import random
-import re
-import sys
+# Python program for Bitonic Sort. Note that this program 
+# works only when size of input is a power of 2. 
+# The parameter dir indicates the sorting direction, ASCENDING 
+# or DESCENDING; if (a[i] > a[j]) agrees with the direction, 
+# then a[i] and a[j] are interchanged.*/ 
+def compAndSwap(a, i, j, dire):
+    if (dire == 1 and a[i] > a[j]) or (dire == 0 and a[i] < a[j]):
+        a[i], a[j] = a[j], a[i]
+        # It recursively sorts a bitonic sequence in ascending order,
+# if dir = 1, and in descending order otherwise (means dir=0).
+# The sequence to be sorted starts at index position low, 
+# the parameter cnt is the number of elements to be sorted. 
+def bitonicMerge(a, low, cnt, dire):
+    if cnt > 1:
+        k = int(cnt / 2)
+        for i in range(low, low + k):
+            compAndSwap(a, i, i + k, dire)
+        bitonicMerge(a, low, k, dire)
+        bitonicMerge(a, low + k, k, dire)
+
+        # This funcion first produces a bitonic sequence by recursively
 
 
-class Car:
+# sorting its two halves in opposite sorting orders, and then
+# calls bitonicMerge to make them in the same order 
+def bitonicSort(a, low, cnt, dire):
+    if cnt > 1:
+        k = int(cnt / 2)
+        bitonicSort(a, low, k, 1)
+        bitonicSort(a, low + k, k, 0)
+        bitonicMerge(a, low, cnt, dire)
 
-    def __new__(cls, max_speed,unit):
-        return "Car with the maximum speed of {0} {1}".format(max_speed, unit)
-
-class Boat:
-
-    def __new__(cls,max_speed):
-        return "Boat with the maximum speed of {0} knots".format(max_speed)
+        # Caller of bitonicSort for sorting the entire array of length N
 
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    q = int(input())
-    queries = []
-    for _ in range(q):
-        args = input().split()
-        vehicle_type, params = args[0], args[1:]
-        if vehicle_type == "car":
-            max_speed, speed_unit = int(params[0]), params[1]
-            vehicle = Car(max_speed, speed_unit)
-        elif vehicle_type == "boat":
-            max_speed = int(params[0])
-            vehicle = Boat(max_speed)
-        else:
-            raise ValueError("invalid vehicle type")
-        fptr.write("%s\n" % vehicle)
-    fptr.close()
+# in ASCENDING order
+def sort(a, N, up):
+    bitonicSort(a, 0, N, up)
+
+
+# Driver code to test above
+a = []
+print("How many numbers u want to enter?");
+n = int(input())
+print("Input the numbers:");
+for i in range(n):
+    a.append(int(input()))
+up = 1
+
+sort(a, n, up)
+print("\n\nSorted array is:")
+for i in range(n):
+    print("%d" % a[i])
