572812b6287c46d388d9815c7e38a00882bcc931
4bhishek10 <abhishek.dhondge10@gmail.com>
2022-04-02 11:07:31 +0530

    working password code

--- Assignment set/Accenture/password.py
+++ Assignment set/Accenture/password.py
@@ -1,25 +1,31 @@
 def upperCase(st):
   for ele in st:
     if ele.isupper():
-      return True
+        #print("true")
+        return True
+        break
   return False 
 
 def containsNumber(st):
   for i in range(0,10):
     j = str(i)
     if j in st:
-      return True
+        #print("true")
+        return True
+        break
   return False
 
 def length(st):
-  if len(str)>=3:
-    return True
+  if len(st)>=3:
+      #print("true")
+      return True
   return False
 
 def space(st):
   for ele in st:
     if ele == ' ' or ele == '/':
       return True
+  #print("true")
   return False
   
 def checkPassword(st):
